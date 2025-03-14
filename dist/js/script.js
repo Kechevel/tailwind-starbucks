025a841c24a0d2d502f0b3e0b05a160eb6e013a7
vincenteeeei <vicenteobiar@gmail.com>
2025-03-14 09:22:25 +0800

    updated today

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,3 +14,27 @@ links.forEach((link) => {
         link.nextElementSibling.classList.toggle("open");
     });
 });
+
+//coffee
+const starNav = document.querySelectorAll(".coffee__nav__link")
+const starContent = document.querySelectorAll(".coffee__item")
+starNav.forEach((star) => {
+    star.addEventListener("click", () =>{
+        removeActiveStar();
+        star.classList.add("active");
+        const activeContent = document.querySelector(`#${star.id}-content`);
+        removeActiveContent();
+        activeContent.classList.add("active");
+    });
+});
+
+function removeActiveStar(){
+    starNav.forEach((star) => {
+        star.classList.remove("active");
+    });
+}
+function removeActiveContent(){
+    starContent.forEach((content) => {
+        content.classList.remove("active");
+    });
+}
